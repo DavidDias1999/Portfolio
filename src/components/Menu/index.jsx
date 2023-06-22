3ca5705bc8f9fc90d998c4c86ad776d0d99af89f
DavidDias1999 <david.vidt@gmail.com>
2023-06-22 11:18:35 -0300

    Header

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,29 @@
+import styles from "./Menu.module.css";
+import MenuLink from "../MenuLink";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { grey } from "@mui/material/colors";
+
+export default function Menu() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/">Projetos</MenuLink>
+        <MenuLink to="/">Formação</MenuLink>
+      </nav>
+      <section className={styles.icon__Wrapper}>
+        <a href="https://github.com/DavidDias1999" target="_blank">
+          <GitHubIcon sx={{ color: grey[50] }} className={styles.icon} />
+        </a>
+        <a href="https://www.linkedin.com/in/daviddias99/" target="_blank">
+          <LinkedInIcon sx={{ color: grey[50] }} className={styles.icon} />
+        </a>
+        <a href="https://bit.ly/3NoDpLa" target="_blank">
+          <WhatsAppIcon sx={{ color: grey[50] }} className={styles.icon} />
+        </a>
+      </section>
+    </header>
+  );
+}
