c1217f4326d6aaa54d05c1a40ca7ae3525293715
DavidDias1999 <david.vidt@gmail.com>
2023-06-23 16:46:22 -0300

    Add Components

---
+++ src/components/ProjectsPreview/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./ProjectsPreview.module.css";
+
+import challenge from "../../assets/images/challenge1.png";
+
+export default function ProjectsPreview() {
+  return (
+    <>
+    </>
+  );
+}
---
+++ src/pages/Projects/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Project.module.css"
+
+
+export default function Projects(){
+  return(
+    <section className={styles.projectsWrapper}>
+      
+
+    </section>
+  )
+}
